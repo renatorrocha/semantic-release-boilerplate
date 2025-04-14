46dd3eefde20b7a7a647f6318beac716a0ce4fa1
renatorrocha <renatorrodrigues2002@gmail.com>
2025-04-14 15:51:59 -0300

    refactor: test semantic-release workflow

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
-console.log("This will be released in v1.0.1");
-
-console.log("test action 3");
-
-// i forgot that "chore" is not a valid semantic release type 😁
-// so i'm just revert the last commit
+console.log("test action");
